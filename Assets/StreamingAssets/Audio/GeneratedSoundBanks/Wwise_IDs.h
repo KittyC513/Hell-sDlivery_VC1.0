3cc67dba79be3a6049ff6c6c94c27aa8af1dff52
KittyC513 <shuichunchenkitty@gmail.com>
2025-04-20 18:00:57 -0400

    Updated

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,101 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_APARTMENT_COMPLEX_V1 = 359343595U;
+        static const AkUniqueID PLAY_BADGESEQUENCE = 3272596896U;
+        static const AkUniqueID PLAY_BELLDING = 1089375233U;
+        static const AkUniqueID PLAY_BOXOPEN = 2128467797U;
+        static const AkUniqueID PLAY_CARTOONFIGHT = 4008188558U;
+        static const AkUniqueID PLAY_CHECKPOINT_GET = 1359342141U;
+        static const AkUniqueID PLAY_CITYLEVELFINAL = 4177256649U;
+        static const AkUniqueID PLAY_CROWNSEQUENCE = 2686563660U;
+        static const AkUniqueID PLAY_DEVIL_THEME = 2640074606U;
+        static const AkUniqueID PLAY_DIZZY = 1634870884U;
+        static const AkUniqueID PLAY_FAN_ALL = 452465945U;
+        static const AkUniqueID PLAY_FIGHTCLANK1 = 1236609236U;
+        static const AkUniqueID PLAY_FIGHTCLANK2 = 1236609239U;
+        static const AkUniqueID PLAY_HAT_REALIZATION_REVERB = 2182026627U;
+        static const AkUniqueID PLAY_HIGHUPAMBIENCE = 1431564541U;
+        static const AkUniqueID PLAY_HUB_MUSIC = 4050862303U;
+        static const AkUniqueID PLAY_LANDINGS = 1019246482U;
+        static const AkUniqueID PLAY_MAIL = 1193902087U;
+        static const AkUniqueID PLAY_METALLANDING = 2180111060U;
+        static const AkUniqueID PLAY_METALSTEPS = 296643996U;
+        static const AkUniqueID PLAY_PACKAGEIMPACT = 246128500U;
+        static const AkUniqueID PLAY_PACKAGEPICK = 3361946317U;
+        static const AkUniqueID PLAY_PACKAGETHROW = 1549093586U;
+        static const AkUniqueID PLAY_PARACHUTE = 419517223U;
+        static const AkUniqueID PLAY_PHONERING = 3447864244U;
+        static const AkUniqueID PLAY_PLATFORMSMOVE = 2752324885U;
+        static const AkUniqueID PLAY_PLATFORMSPHASEIN = 2281964030U;
+        static const AkUniqueID PLAY_PLATFORMSPHASEOUT = 123604001U;
+        static const AkUniqueID PLAY_PUSHCHARGE = 1312932426U;
+        static const AkUniqueID PLAY_PUSHRELEASE = 3357210845U;
+        static const AkUniqueID PLAY_RANDWOODLAND = 415850775U;
+        static const AkUniqueID PLAY_SCORECARD_SEQUENCE = 342527874U;
+        static const AkUniqueID PLAY_SCORESCENEMUSIC = 613185981U;
+        static const AkUniqueID PLAY_SHMINKDEATH = 3547701186U;
+        static const AkUniqueID PLAY_SHMINKJUMP = 2298361104U;
+        static const AkUniqueID PLAY_SHMINKPUSHEDR = 3885831539U;
+        static const AkUniqueID PLAY_SHMINKTHROW = 1350790884U;
+        static const AkUniqueID PLAY_SHMONKDEATH = 1576822420U;
+        static const AkUniqueID PLAY_SHMONKJUMP = 2195620242U;
+        static const AkUniqueID PLAY_SHMONKTHROW = 1729994058U;
+        static const AkUniqueID PLAY_SMONKPUSHED = 4031885603U;
+        static const AkUniqueID PLAY_SNIFF = 913401646U;
+        static const AkUniqueID PLAY_SPARKLE = 3595246800U;
+        static const AkUniqueID PLAY_SQUEAKYBITE = 3018288467U;
+        static const AkUniqueID PLAY_STINKYSOUND1 = 1712731588U;
+        static const AkUniqueID PLAY_STRETCH = 1150790929U;
+        static const AkUniqueID PLAY_SUMMONING_CIRCLE_ALL = 2914340836U;
+        static const AkUniqueID PLAY_TUTORIALMUSIC = 2086825057U;
+        static const AkUniqueID PLAY_WHOOSH = 148817996U;
+        static const AkUniqueID PLAY_WINDCATCH = 3366204753U;
+        static const AkUniqueID PLAY_WOODSTEPS = 1787223838U;
+        static const AkUniqueID PLAY_YAPPING = 3171885004U;
+        static const AkUniqueID PLAYER_STEPS = 4272057794U;
+        static const AkUniqueID STOP_ALL = 452547817U;
+        static const AkUniqueID STOP_COMICSFX = 4150189060U;
+        static const AkUniqueID STOP_FAN_ALL = 3708642647U;
+        static const AkUniqueID STOP_PARACHUTE = 2017739505U;
+        static const AkUniqueID STOP_PUSHCHARGE = 22663848U;
+        static const AkUniqueID STOP_SUMMONING_CIRCLE_ALL = 222428266U;
+        static const AkUniqueID STOP_WINDCATCH = 1676944771U;
+        static const AkUniqueID STOP_YAPPING = 1587897726U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PLAYERSFX = 4283257371U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID REVERBS = 3545700988U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
